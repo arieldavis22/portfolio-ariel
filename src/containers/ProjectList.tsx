f03bc602b2de7d2b456d3d54fb5ed96f8ea7d7e2
Ariel Davis <amanichai100@gmail.com>
2022-09-12 20:36:42 -0400

    grid

--- src/containers/ProjectList.tsx
+++ src/containers/ProjectList.tsx
@@ -2,22 +2,24 @@ import { useState } from "react";
 import data from "../data.json";
 import ProjectModule from "../components/ProjectModule";
 import MainText from "../components/MainText";
+import "../stylesheets/project-list.css";
 
 const ProjectList: React.FC = () => {
   const [mainText, setMainText] = useState({
-    header: "Default",
-    text: "default",
+    header: "Ariel Davis",
+    text: "some random default text",
   });
   return (
-    <div>
-      <h3>List of Projects</h3>
-      {/* Map through Projects */}
-      {/* Have each project be a component (Project)
-      and take in the project object, and a click event (to change the MainText obj) as props */}
-      {data.projects.map((project) => (
-        <ProjectModule project={project} handler={setMainText} />
-      ))}
-      <MainText data={mainText} />
+    <div className="project-list">
+      <div>
+        <MainText data={mainText} />
+      </div>
+      <div>
+        <h3>List of Projects</h3>
+        {data.projects.map((project) => (
+          <ProjectModule project={project} handler={setMainText} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./containers/App";
+import "./stylesheets/index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="main">
+      <App />
+    </div>
   </React.StrictMode>
 );
